69287107ad817ab3e6ef9208b9a0420f29d95ff6
Suraj Suryawanshi <surajsuryawanshi2424@gmail.com>
2025-10-31 18:21:48 +0530

    fourth commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ allowed_origins = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://quick-poll-azure-six.vercel.app"
 ]
 
 app.add_middleware(
